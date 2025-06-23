2aa0a36c1958ff4d0a83783a81bb709e6d2e21ee
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2025-06-23 14:29:51 +0300

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 <section className="about-me">
   <p>
     I am a Backend  web developer with over 1.5 year of experience and a computer science graduate. 
-    I have completed more than 60 projects in web development, including over +10 live projects for actual clients 
+    I have completed more than 60 projects in web development, including over +13 live projects for actual clients 
     during my work experience, from start to finish.
   </p>
   <button 
