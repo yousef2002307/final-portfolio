657f1e9889b03f7eff114ab327137b9c644899e9
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2026-07-24 17:12:51 +0300

    feat: add API endpoint for logging visitor interactions

--- api/log-visit.js
+++ api/log-visit.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
   const now = new Date().toUTCString();
 
   const ecHeader = ecParam ? [
-    '🏢 🔥 *[ ' + ecParam + ' HAS ENTERED! ]* 🔥 🏢',
+    `🏢 🔥 *[ ${ecParam} HAS ENTERED! ]* 🔥 🏢`,
     ''
   ] : [];
 
